3f662229342cec0a637129509ad428333f598918
eAdnan007 <adnan007.id@gmail.com>
2014-05-25 03:59:09 +0600

    Added comments and documentations.

--- README.md
+++ README.md
@@ -1,4 +1,70 @@
 jquery-countdown
 ================
 
-A jquery countdown timer with progressbar
+It is a very simple and light weight jQuery countdown plugin that supports progressbar and count from days to seconds.
+
+#Usage
+
+##Files to load
+You only need to load jQuery and this plugin. For example:
+
+```html
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="https://rawgit.com/eAdnan007/jquery-countdown/master/jquery.countdown.min.js"></script>
+```
+
+##Invoke
+On a dom element, call method countdown() with/without your preferences.
+
+```js
+$('div').countdown({
+	end_time: "2014-04-15T22:45+06:00"
+});
+```
+
+#Supported Properties
+Following properties are supported:
+`start_time`, `end_time`, `show_day`, `show_hour`, `show_minute`, `show_second`, `update_int`, `progress`, `onComplete`, `wrapper`, `update_progress`
+
+##start_time
+Specifies a time where the progressbar should have no progress(zero). You can pass any datetime string that is supported by javascript Data() object. However, I recommend the format, `YYYY-MM-DDThh:mmTZD`. Where,
+YYYY	= Year
+MM		= Month
+DD		= Day of month
+hh		= Hour in 24 hour format
+mm		= Minutes
+TZD		= Timezone offset
+
+Example:
+`2014-05-25T13:34+06:00`
+
+##end_time
+The countdown completes at this point and progressbar fills 100%. It support same input is `start_time`.
+
+##show_day
+Wheather to show day count or not.
+
+##show_hour
+Wheather to show hour count or not.
+
+##show_minute
+Wheather to show minute count or not.
+
+##show_second
+Wheather to show second count or not.
+
+##update_int
+Interval between each updates of progressbar and timer.
+
+##progress
+There dom element which should display the progressbar. False if you don't want to display.
+
+##onComplete
+Function to trigger when countdown is done.
+
+##wrapper
+Modify the outer html of each countdown unit. This function receives a single argument, the name of the unit(Day, Hour, Minute, Second).
+`Note: It must have an element with class counter; where the count will be displayed.`
+
+##update_progress
+Function to process the progress for the preogress bar. It receives two arguments, `Percentage of progress (0-100)` and `The dom element containing the progressbar`.
--- jquery.countdown.js
+++ jquery.countdown.js
@@ -7,8 +7,10 @@
  */
 (function($){
 	$.fn.countdown = function(options){
+		//Variables that we are going to use
 		var dwcd_day, dwcd_hour, dwcd_minute, dwcd_second, dwcd_start_time, dwcd_end_time, dwcd_complete = false;
 		
+		//Set the default settings
 		var settings = $.extend({
 			start_time		: null,
 			end_time		: null,
@@ -27,18 +29,22 @@
 				return wrapper;
 			},
 			update_progress	: function(progress, element){
-				element.attr('aria-valuenow', Math.floor(progress));
-				element.css('width', Math.floor(progress)+'%');
-				element.text(Math.floor(progress)+'%');
+				element.attr('aria-valuenow', Math.floor(progress));//We set a custom attribute, 'area-valuenow' containing the progress
+				element.css('width', Math.floor(progress)+'%');//Fill the bar with percentage of progress
+				element.text(Math.floor(progress)+'%');//Put text notation of progress inside the progressbar
 			}
 		}, options);
 
+		//If start time is not set, set it to current time.
 		if(settings.start_time == null) dwcd_start_time = new Date();
 		else dwcd_start_time = new Date(settings.start_time);
 
+		//If end time is not set(silly), set it to some time tomorrow.
 		if(settings.end_time == null) dwcd_end_time = new Date(new Date() + 25 * 60 * 60 * 1000);
 		else dwcd_end_time = new Date(settings.end_time);
 
+
+		//Check each countdown unit if it is set to display and create html element to display them.
 		if(settings.show_day)
 			dwcd_day = settings.wrapper('Day');
 		if(settings.show_hour)
@@ -48,22 +54,29 @@
 		if(settings.show_second)
 			dwcd_second = settings.wrapper('Second');
 
-		update();
+
+		//Update the countdown timer and progressbar on set interval.
+		update();//First time it should run without wait.
 		setInterval(update, settings.update_int*1000);
 
+
+		//We get every countdown element's html inside the main element.
 		this.append(dwcd_day);
 		this.append(dwcd_hour);
 		this.append(dwcd_minute);
 		this.append(dwcd_second);
+
 
 		function update(){
 
 			var now		= new Date();
 			var left	= dwcd_end_time - now;
 
+			//How much time is left?
 			left = Math.floor(left / 1000);
-			if(left < 1) left = 0;
+			if(left < 1) left = 0;//We don't want to count negatives!
 
+			//If no time is left it is complete.
 			if(!dwcd_complete){
 				if(left == 0){
 					dwcd_complete = true;
@@ -71,36 +84,41 @@
 				}
 			}
 
+			//Extract the days and keep remaining hours in the variable 'left'
 			var days	= Math.floor(left / (60 * 60 * 24));
 			if(settings.show_day){
 				dwcd_day.children('.counter').text(days);
 				left = left % (60 * 60 * 24);
 			}
 			
+			//Extract the hours and keep remaining minutes in the variable 'left'
 			var hours	= Math.floor(left / (60 * 60));
 			if(settings.show_hour){
 				dwcd_hour.children('.counter').text(hours);
 				left = left % (60 * 60);
 			}
 			
+			//Extract the minutes and keep remaining seconds in the variable 'left'
 			var minutes	= Math.floor(left / 60);
 			if(settings.show_minute){
 				dwcd_minute.children('.counter').text(minutes);
 				left = left % 60;
 			}
 			
+			//Whatever we have left is seconds.
 			var seconds	= Math.floor(left);
 			if(settings.show_second){
 				dwcd_second.children('.counter').text(seconds);
 			}
 
+			//If progressbar is set to display, get in to update it.
 			if(settings.progress != false){
-				var total_time	= Math.round(dwcd_end_time - dwcd_start_time);
-				var spent_time	= Math.round(now - dwcd_start_time);
-				var progress	= spent_time/total_time*100;
-				if(progress > 100)progress = 100;
+				var total_time	= Math.round(dwcd_end_time - dwcd_start_time);//How much time in total
+				var spent_time	= Math.round(now - dwcd_start_time);//How much time already spent
+				var progress	= spent_time/total_time*100;//Percentage of progress
+				if(progress > 100)progress = 100;//Progress should never be more than 100%
 				
-				settings.update_progress(progress, settings.progress);
+				settings.update_progress(progress, settings.progress);//Call the progress update function
 			}
 		}
 	}
